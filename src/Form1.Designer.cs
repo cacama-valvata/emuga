c261a877ef088523a6b033af2619ba2ee65405b7
Casey Colley <casey.colley@biamp.com>
2022-06-23 16:42:47 -0700

    bare bones UI for displaying information from mod file

--- src/Form1.Designer.cs
+++ src/Form1.Designer.cs
@@ -28,12 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.TopMenuBar = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenButton = new System.Windows.Forms.ToolStripMenuItem();
+            this.ExitButton = new System.Windows.Forms.ToolStripMenuItem();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.OpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.TopMenuBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.TopMenuBar, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.StatusLabel, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.692307F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 92.30769F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // TopMenuBar
+            // 
+            this.TopMenuBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.TopMenuBar.Location = new System.Drawing.Point(0, 0);
+            this.TopMenuBar.Name = "TopMenuBar";
+            this.TopMenuBar.Size = new System.Drawing.Size(800, 24);
+            this.TopMenuBar.TabIndex = 0;
+            this.TopMenuBar.Text = "TopMenuBar";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.OpenButton,
+            this.ExitButton});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(180, 22);
+            this.OpenButton.Text = "Open...";
+            this.OpenButton.Click += new System.EventHandler(this.OpenButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(180, 22);
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.StatusLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.StatusLabel.Location = new System.Drawing.Point(3, 33);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Padding = new System.Windows.Forms.Padding(10);
+            this.StatusLabel.Size = new System.Drawing.Size(794, 396);
+            this.StatusLabel.TabIndex = 1;
+            this.StatusLabel.Text = "No MOD file open.";
+            // 
+            // OpenFileDialog
+            // 
+            this.OpenFileDialog.Filter = "MOD files|*.mod|All files|*.*";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.TopMenuBar.ResumeLayout(false);
+            this.TopMenuBar.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private MenuStrip TopMenuBar;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem OpenButton;
+        private ToolStripMenuItem ExitButton;
+        private OpenFileDialog OpenFileDialog;
+        private Label StatusLabel;
     }
 }
--- src/Form1.cs
+++ src/Form1.cs
@@ -2,9 +2,26 @@ namespace emuga
 {
     public partial class Form1 : Form
     {
+        public string? FileName = null;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void OpenButton_Click(object sender, EventArgs e)
+        {
+            if (OpenFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                FileName = OpenFileDialog.FileName;
+
+                StatusLabel.Text = "MOD file loaded. Please wait while it is being processed...";
+            }
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
